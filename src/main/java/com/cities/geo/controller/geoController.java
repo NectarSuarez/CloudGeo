4252554eb4aabfe2d6bcf2ababb56728b14c4620
NectarSuarez <33638403+NectarSuarez@users.noreply.github.com>
2022-03-11 12:14:31 -0500

    20220311
    Mejoras en nube

--- src/main/java/com/cities/geo/controller/geoController.java
+++ src/main/java/com/cities/geo/controller/geoController.java
@@ -3,9 +3,7 @@ package com.cities.geo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +20,6 @@ public class geoController
 	@Autowired
 	geoService geoService;
 	
-	//@PostMapping(value = "/Test", produces = MediaType.APPLICATION_JSON_VALUE)
 	@RequestMapping(value = "/Test", method = RequestMethod.GET)
 	public int MultiSinSigno(@RequestParam("a")int A, @RequestParam("b") int B) throws Exception
 	{
