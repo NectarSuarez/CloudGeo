21c1cc61e7559dcfd00b49fe622258a7b6b741eb
NectarSuarez <33638403+NectarSuarez@users.noreply.github.com>
2022-03-11 13:45:07 -0500

    Fourth
    Fourth

--- src/main/java/com/cities/geo/CitiesApplication.java
+++ src/main/java/com/cities/geo/CitiesApplication.java
@@ -2,12 +2,14 @@ package com.cities.geo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class CitiesApplication {
+public class CitiesApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitiesApplication.class, args);
